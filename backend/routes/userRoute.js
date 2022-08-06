83007ec76187dddadd4ed4f670fa6002e80ed210
natferlima <ferlima.natalia@gmail.com>
2022-08-05 23:32:54 -0300

    feat: complete userRoute

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,9 @@ const { userController } = require('../controllers');
 
 const router = express.Router();
 
+router.post('/', userController.create)
 router.get('/', userController.getAll);
+router.get('/:id', userController.getOne);
+router.delete('/:id', userController.remove);
 
 module.exports = router;
