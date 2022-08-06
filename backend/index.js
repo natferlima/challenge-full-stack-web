20309fe63e6ea65015ea4dccb0caff8ffc1f56c9
natferlima <ferlima.natalia@gmail.com>
2022-08-05 22:23:43 -0300

    feat: add routes folder and userRoute

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const { userRouter } = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello!');
-})
+app.use('/user', userRouter);
 
 const PORT = 3001;
 app.listen(3001, () => {
---
+++ backend/routes/index.js
@@ -0,0 +1,3 @@
+const userRouter = require('./userRoute');
+
+module.exports = { userRouter };
